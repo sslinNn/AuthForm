37c39cdf8229a5d5821f148a9aa7b62aafd9efbe
sslinNn <morison1991@mail.ru>
2023-05-13 01:19:59 +0700

    1
    1

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+
+db = SQLAlchemy(app=app)
 
 @app.route('/')
 def index():
