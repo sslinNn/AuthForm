97658dd8867f483bdc2df54795ae7ba5ca0194d5
sslinNn <morison1991@mail.ru>
2023-05-12 23:45:45 +0700

    1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-print('Hello')
