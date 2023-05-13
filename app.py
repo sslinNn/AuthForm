f26117405ef2692158c46ea6b3b16fae77f31e55
sslinNn <morison1991@mail.ru>
2023-05-13 22:33:17 +0700

    add some shit

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, g
+from FDataBase import FDataBase
+import sqlite3
+import os
+
+# Config
+DATABASE = '/tmp/site.db'
+SECRET_KEY = '23wqe12dcdsv24jgm0=im=K+)((M3102'
+DEBUG = True
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'site.db')))
 
-db = SQLAlchemy(app=app)
+# Функция для подключения к БД
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
 
+# Функция для создания БД
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+# Функция для поключения БД к WEB
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+
+# Функция для закрытия БД
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+"""----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------"""
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template('home.html')
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('home.html', menu=dbase.getMenu())
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ sq_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS mainmenu(
+    ID integer PRIMARY KEY AUTOINCREMENT,
+    Title text NOT NULL,
+    url text NOT NULL
+);
